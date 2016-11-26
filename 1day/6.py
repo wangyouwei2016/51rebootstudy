68fd2b12a0a92cab312ee8adfc2eb3d3d4c30288
wangyw <13709821410@139.com>
2016-11-26 15:31:20 +0800

    1

--- 1day/6.py
+++ 1day/6.py
@@ -3,3 +3,9 @@ for name in ['c','js','py','js']:
     if name=='js':
         js_count +=1
 print js_count
+
+max_num =0
+for i in [1,2,3,4,4,5,777,6666,999]:
+    if i>max_num:
+        max_num=i
+print max_num
