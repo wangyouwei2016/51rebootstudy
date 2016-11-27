16b23ec6da4e99f11ce1ec5ff55c8d80fb6279df
wangyw <13709821410@139.com>
2016-11-27 23:15:32 +0800

    youhua

--- 1day/6.py
+++ 1day/6.py
@@ -10,17 +10,14 @@ for i in [1,2,3,4,4,5,777,6666,999]:
         max_num=i
 print max_num
 
-max_num1=0
-max_num2=0
+
 
 for i in [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]:
     if i > max_num1:
         max_num1=i
 print(max_num1)
 
-unsortedList=[]
-unsortedList=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
-print sorted(unsortedList)
 
-print unsortedList.sort()
-print unsortedList  
+unsortedList=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+print sorted(unsortedList)[-2:]
+
