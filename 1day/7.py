1920f6eb006f2478b2a5f4a1580600abce79ebd6
wangyw <13709821410@139.com>
2016-11-28 16:40:00 +0800

    1128

--- 1day/7.py
+++ 1day/7.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-  
 
 # http://blog.csdn.net/onlyanyz/article/details/45177643  参考的算法讲解
-# 选中 ctrl+/ 批量注释  pycharm技巧
+# pycharm技巧：  选中 ctrl+/ 批量注释  ctrl+d  快速复制  ctrl+e 快速删除 shift+enter 快速换行  TAB缩进  有批量tab转空格功能
 #作业1 统计最大两个值
 #方法1
 unsortedList=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
