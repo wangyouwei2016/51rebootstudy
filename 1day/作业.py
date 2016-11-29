d53c96d4728ee86b4e3556c44a19bd1a71772cc1
wangyw <13709821410@139.com>
2016-11-29 13:49:11 +0800

    1129

--- 1day/作业.py
+++ 1day/作业.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # http://blog.csdn.net/onlyanyz/article/details/45177643  参考的算法讲解
-# 选中 ctrl+/ 批量注释  pycharm技巧
+# pycharm技巧：  选中 ctrl+/ 批量注释  ctrl+d  快速复制  ctrl+e 快速删除 shift+enter 快速换行  ctrl+alt+l 遵守PEP8编码规范的格式化
 # 方法1
 
 
@@ -26,3 +26,18 @@ unsortedList = [1 , 2 , 3 , 2 , 12 , 3 , 1 , 3 , 21 , 2 , 2 , 3 , 4111 , 22 , 33
                 45 , 33 , 45]
 for i in sorted(unsortedList)[-2:]:
     print(i)
+
+# 同学的
+
+# coding=utf-8
+a = [1 , 2 , 3 , 2 , 12 , 3 , 1 , 3 , 21 , 2 , 2 , 3 , 4111 , 22 , 3333 , 444 , 111 , 4 , 5 , 777 , 65555 , 45 , 33 ,
+     45]
+max_num = 0
+min_num = 0
+for i in a:
+    if i > max_num:
+        min_num = max_num
+        max_num = i
+    elif i > min_num:
+        min_num = i
+print "最大的两个值为:%s,%s" % (min_num , max_num)
