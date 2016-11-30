b70a77c5afe978447c114b4a2ca0eb1a14d73a9a
wangyw <13709821410@139.com>
2016-11-30 08:43:04 +0800

    1129

--- 1day/作业.py
+++ 1day/作业.py
@@ -41,3 +41,15 @@ for i in a:
     elif i > min_num:
         min_num = i
 print "最大的两个值为:%s,%s" % (min_num , max_num)
+
+
+_list=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+max1=0
+max2=0
+for i in _list:
+	for j in _list:
+		if max1 < i:
+			max1=i
+		if max1 > j and j > max2:
+			max2=j
+print 'The first big number: %s. Second big numbers: %s' %(max1,max2)
