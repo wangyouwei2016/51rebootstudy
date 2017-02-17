dfc28b2d2411cbb31fc3a2faff5ca93b94922876
wangyw <13709821410@139.com>
2017-02-17 16:39:35 +0800

    1204

--- 1day/1.py
+++ 1day/1.py
@@ -1,41 +1,39 @@
-#
-# Custom awk.py module
-#
+#coding=utf-8
+import MySQLdb
+conn = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user='root',
+                passwd='weiwei',
+                db='yp',
+                charset='utf8',
+            )
+cur = conn.cursor()
+f = open("/home/wangyw/yp.txt", "r")
+while True:
+    line = f.readline()
+    if line:
+        #处理每行\n
+        line = line.strip('\n')
+        line = line.split(":")
+        print (line)
+        # if line in 'uuid':
+        #     print (line)
+        # uuid = line[1]
+        # print uuid
 
 
-class controller:
-    def __init__(self , f):
-        self.m_file = f
-        self.m_handlers = []
 
-    def subscribe(self , o):
-        self.m_handlers.append(o)
 
-    def run(self):
-
-        for o in self.m_handlers:
-            o.begin()
-
-        s = self.m_file.readline()
-
-        while s != "":
-
-            for o in self.m_handlers:
-                o.process_line(s)
-
-            s = self.m_file.readline()
-
-        for o in self.m_handlers:
-            o.end()
-
-    def print_results(self):
-
-        print
-        print "Results:"
-        print
-
-        for o in self.m_handlers:
-            print "------------------------------------------------------"
-            print o.description()
-            print "------------------------------------------------------"
-            print o.result()
+#         province = line[1]
+#         city = line[2]
+#         call_type = line[3]
+#         cur.execute(
+#             "insert into data9(tel,province,city,cell_type) values(%s,%s,%s,%s)",
+#             [tel, province, city, call_type])
+    else:
+        break
+# f.close()
+# cur.close()
+# conn.commit()
+# conn.close()
